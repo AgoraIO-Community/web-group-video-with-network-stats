af55b9c57ed2f67008c47d6c00590a2b26245c41
Hermes <hermes.frangoudis@gmail.com>
2020-11-10 22:15:14 -0500

    added stats

--- agora-interface.js
+++ agora-interface.js
@@ -4,7 +4,7 @@
 
 // video profile settings
 var cameraVideoProfile = '480p_4'; // 640 × 480 @ 30fps  & 750kbs
-var screenVideoProfile = '480p_2'; // 640 × 480 @ 30fps
+var screenVideoProfile = '720p_2'; // 640 × 480 @ 30fps
 
 // create client instances for camera (client) and screen share (screenClient)
 var client = AgoraRTC.createClient({mode: 'rtc', codec: 'vp8'}); 
@@ -24,6 +24,8 @@ var localStreams = {
   }
 };
 
+var statsIntervals = []; // references to intervals for getting in-call stats
+
 var mainStreamId; // reference to main stream
 var screenShareActive = false; // flag for screen share 
 
@@ -40,6 +42,7 @@ function initClientAndJoinChannel(agoraAppId, token, channelName, uid) {
 
 client.on('stream-published', function (evt) {
   console.log("Publish local stream successfully");
+  enableStats();
 });
 
 // connect remote streams
@@ -65,6 +68,7 @@ client.on('stream-subscribed', function (evt) {
   if( $('#full-screen-video').is(':empty') ) { 
     mainStreamId = remoteId;
     remoteStream.play('full-screen-video');
+    $('#main-stats-btn').show();
   } else {
     addRemoteStreamMiniView(remoteStream);
   }
@@ -78,12 +82,16 @@ client.on("peer-leave", function(evt) {
     delete remoteStreams[streamId]; // remove stream from list
     if (streamId == mainStreamId) {
       var streamIds = Object.keys(remoteStreams);
-      var randomId = streamIds[Math.floor(Math.random()*streamIds.length)]; // select from the remaining streams
-      remoteStreams[randomId].stop(); // stop the stream's existing playback
-      var remoteContainerID = '#' + randomId + '_container';
-      $(remoteContainerID).empty().remove(); // remove the stream's miniView container
-      remoteStreams[randomId].play('full-screen-video'); // play the random stream as the main stream
-      mainStreamId = randomId; // set the new main remote stream
+      if (streamIds.length > 1) {
+        var randomId = streamIds[Math.floor(Math.random()*streamIds.length)]; // select from the remaining streams
+        remoteStreams[randomId].stop(); // stop the stream's existing playback
+        var remoteContainerID = '#' + randomId + '_container';
+        $(remoteContainerID).empty().remove(); // remove the stream's miniView container
+        remoteStreams[randomId].play('full-screen-video'); // play the random stream as the main stream
+        mainStreamId = randomId; // set the new main remote stream
+      } else {
+        $('#main-stats-btn').hide();
+      }
     } else {
       var remoteContainerID = '#' + streamId + '_container';
       $(remoteContainerID).empty().remove(); // 
@@ -172,7 +180,7 @@ function joinChannelAsScreenShare(channelName) {
     // Create the stream for screen sharing.
     var screenStream = AgoraRTC.createStream({
       streamID: uid,
-      audio: false, // Set the audio attribute as false to avoid any echo during the call.
+      audio: false,
       video: false,
       screen: true, // screen stream
       screenAudio: true,
@@ -199,11 +207,15 @@ function joinChannelAsScreenShare(channelName) {
 
   screenClient.on('stream-published', function (evt) {
     console.log("Publish screen stream successfully");
-    localStreams.camera.stream.disableVideo(); // disable the local video stream (will send a mute signal)
-    localStreams.camera.stream.stop(); // stop playing the local stream
+    // localStreams.camera.stream.disableVideo(); // disable the local video stream (will send a mute signal)
+    // localStreams.camera.stream.stop(); // stop playing the local stream
     // TODO: add logic to swap main video feed back from container
-    remoteStreams[mainStreamId].stop(); // stop the main video stream playback
-    addRemoteStreamMiniView(remoteStreams[mainStreamId]); // send the main video stream to a container
+    if(mainStreamId){
+      remoteStreams[mainStreamId].stop(); // stop the main video stream playback
+      addRemoteStreamMiniView(remoteStreams[mainStreamId]); // send the main video stream to a container
+    }
+    mainStreamId = localStreams.screen.id;
+    evt.stream.play('full-screen-video');
     // localStreams.screen.stream.play('full-screen-video'); // play the screen share as full-screen-video (vortext effect?)
     $("#video-btn").prop("disabled",true); // disable the video button (as cameara video stream is disabled)
   });
@@ -237,13 +249,27 @@ function addRemoteStreamMiniView(remoteStream){
   var streamId = remoteStream.getId();
   // append the remote stream template to #remote-streams
   $('#remote-streams').append(
-    $('<div/>', {'id': streamId + '_container',  'class': 'remote-stream-container col'}).append(
+    $('<div/>', { 'id': streamId + '_container', 'class': 'remote-stream-container col' }).append(
       $('<div/>', {'id': streamId + '_mute', 'class': 'mute-overlay'}).append(
           $('<i/>', {'class': 'fas fa-microphone-slash'})
       ),
-      $('<div/>', {'id': streamId + '_no-video', 'class': 'no-video-overlay text-center'}).append(
-        $('<i/>', {'class': 'fas fa-user'})
-      ),
+      $('<div/>', { 'id': streamId + '_no-video', 'class': 'no-video-overlay text-center',}).append(
+          $('<i/>', {'class': 'fas fa-user'})
+        ),
+      $('<div/>', { 'id': streamId + '-stats-container', 'class': 'remote-stats-container col-2 float-right text-right p-0 m-0',}).append(
+          $('<button/>', {
+                          'id': streamId +'-stats-btn', 
+                          'type': 'button', 
+                          'class': 'btn btn-lg',
+                          'data-toggle': 'popover',
+                          'data-placement': 'top',
+                          'data-html': true,
+                          'title': 'Video Stats',
+                          'data-content': 'loading stats...'
+            }).append(
+            $('<i/>', {'class': 'fas fa-info-circle', 'style':'color:#fff'})
+          )
+        ),
       $('<div/>', {'id': 'agora_remote_' + streamId, 'class': 'remote-video'})
     )
   );
@@ -266,6 +292,9 @@ function leaveChannel() {
   if(screenShareActive) {
     stopScreenShare();
   }
+
+  // disable stats interval
+  disableStats(); 
 
   client.leave(function() {
     console.log("client leaves channel");
@@ -292,3 +321,147 @@ function leaveChannel() {
 function generateToken() {
   return null; // TODO: add a token generation
 }
+
+
+// stats
+function enableStats() {
+  // network
+  var networkStatsBtn = $('#network-stats-btn');
+  var networkInterval = setInterval(() => {
+    if(networkStatsBtn.data('bs.popover') && networkStatsBtn.attr('aria-describedby')) {
+      client.getTransportStats((stats) => {
+        var networkStats = `<strong>Round-Trip Time:</strong> ${stats.RTT}<br/>
+                            <strong>Network Type:</strong> ${stats.networkType}<br/>
+                            <strong>Outgoing Available Bandwidth:</strong> ${stats.OutgoingAvailableBandwidth}
+                          `;
+        networkStatsBtn.data('bs.popover').element.dataset.content = networkStats;
+        networkStatsBtn.data('bs.popover').setContent();
+        networkStatsBtn.popover('update');
+      });
+    }
+  }, 1000);                        
+  statsIntervals.network = networkInterval;
+
+  // session
+  var sessionStatsBtn = $('#session-stats-btn');
+  var sessionInterval = setInterval(() => {
+    if(sessionStatsBtn.data('bs.popover') && sessionStatsBtn.attr('aria-describedby')) {
+      client.getSessionStats((stats) => {
+          var sessionStats = `<strong>Duration:</strong> ${stats.Duration}s<br/>
+                              <strong>User Count:</strong> ${stats.UserCount}<br/>
+                              <strong>Sent Bytes:</strong> ${stats.SendBytes}<br/>
+                              <strong>Recv Bytes:</strong> ${stats.RecvBytes}<br/>
+                              <strong>Send Bitrate:</strong> ${stats.SendBitrate} Kbps<br/>
+                              <strong>Recv Bitrate:</strong> ${stats.RecvBitrate} Kbps
+                            `;
+          sessionStatsBtn.data('bs.popover').element.dataset.content = sessionStats;
+          sessionStatsBtn.data('bs.popover').setContent();
+          sessionStatsBtn.popover('update');
+      });
+    }
+  }, 1000);
+  statsIntervals.session = sessionInterval;
+
+  // local audio
+  var localAudioStatsBtn = $('#audio-stats-btn');
+  var localAudioInterval = setInterval(() => {
+    localAudioStatsBtn.show();
+    if(localAudioStatsBtn.data('bs.popover') && localAudioStatsBtn.attr('aria-describedby')) {
+      client.getLocalAudioStats((localAudioStats) => {
+        for(var uid in localAudioStats){
+          if(uid == localStreams.camera.id) {
+            var audioStats = `<strong>Codec Type:</strong> ${localAudioStats[uid].CodecType}<br/>
+                              <strong>Mute State:</strong> ${localAudioStats[uid].MuteState}<br/>
+                              <strong>Recording Level:</strong> ${localAudioStats[uid].RecordingLevel}<br/>
+                              <strong>Sampling Rate:</strong> ${localAudioStats[uid].SamplingRate} kHz<br/>
+                              <strong>Send Bitrate:</strong> ${localAudioStats[uid].SendBitrate} Kbps<br/>
+                              <strong>SendLevel:</strong> ${localAudioStats[uid].SendLevel} 
+                            `;
+            localAudioStatsBtn.data('bs.popover').element.dataset.content = audioStats;
+            localAudioStatsBtn.data('bs.popover').setContent();
+            localAudioStatsBtn.popover('update');
+          }
+        }
+      });
+    }
+  }, 1000);
+  statsIntervals.localAudio = localAudioInterval;
+
+  // local video
+  var localVideoStatsBtn = $('#video-stats-btn');
+  var localVideoInterval = setInterval(() => {
+    localVideoStatsBtn.show();
+    if(localVideoStatsBtn.data('bs.popover')&& localVideoStatsBtn.attr('aria-describedby')) {
+      client.getLocalVideoStats((localVideoStats) => {
+        for(var uid in localVideoStats){
+          if(uid == localStreams.camera.id) {
+            var videoStats = `<strong>Capture Frame Rate:</strong> ${localVideoStats[uid].CaptureFrameRate} fps<br/>
+                              <strong>Capture Resolution Height:</strong> ${localVideoStats[uid].CaptureResolutionHeight}px<br/>
+                              <strong>Capture Resolution Width:</strong> ${localVideoStats[uid].CaptureResolutionWidth}px<br/>
+                              <strong>Encode Delay:</strong> ${localVideoStats[uid].EncodeDelay}ms<br/>
+                              <strong>Mute State:</strong> ${localVideoStats[uid].MuteState}<br/>
+                              <strong>Send Bitrate:</strong> ${localVideoStats[uid].SendBitrate} Kbps<br/>
+                              <strong>Send Frame Rate:</strong> ${localVideoStats[uid].SendFrameRate} fps<br/>
+                              <strong>Send Resolution Heigh:</strong> ${localVideoStats[uid].SendResolutionHeight}px<br/>  
+                              <strong>Send Resolution Width:</strong> ${localVideoStats[uid].SendResolutionWidth}px<br/>
+                              <strong>Target Send Bitrate:</strong> ${localVideoStats[uid].TargetSendBitrate} Kbps<br/>
+                              <strong>Total Duration:</strong> ${localVideoStats[uid].TotalDuration}s<br/>
+                              <strong>Total Freeze Time:</strong> ${localVideoStats[uid].TotalFreezeTime}s 
+                            `;
+            localVideoStatsBtn.data('bs.popover').element.dataset.content = videoStats;
+            localVideoStatsBtn.data('bs.popover').setContent();
+            localVideoStatsBtn.popover('update');
+          }
+        }
+      });
+    }
+
+  }, 1000);
+  statsIntervals.localVideo = localVideoInterval;
+
+  // remote video
+  var remoteVideoInterval = setInterval(() => {
+    client.getRemoteVideoStats((remoteVideoStatsMap) => {
+      for(var uid in remoteVideoStatsMap){
+        var remoteVideoStatsBtn;
+        if(uid == mainStreamId){
+          remoteVideoStatsBtn = $('#main-stats-btn');
+        } else {
+          remoteVideoStatsBtn = $('#'+ uid +'-stats-btn');
+        }
+        if(remoteVideoStatsBtn.data('bs.popover')&& remoteVideoStatsBtn.attr('aria-describedby')) {
+          var videoStats = `<strong>End 2 End Delay:</strong> ${remoteVideoStatsMap[uid].End2EndDelay}ms<br/>
+                            <strong>Mute State:</strong> ${remoteVideoStatsMap[uid].MuteState}<br/>
+                            <strong>Packet Loss Rate:</strong> ${remoteVideoStatsMap[uid].PacketLossRate}%<br/>
+                            <strong>Recv Bitrate:</strong> ${remoteVideoStatsMap[uid].RecvBitrate} Kbps<br/>
+                            <strong>Recv Resolution Height:</strong> ${remoteVideoStatsMap[uid].RecvResolutionHeight}px<br/>
+                            <strong>Recv Resolution Width:</strong> ${remoteVideoStatsMap[uid].RecvResolutionWidth}px<br/>
+                            <strong>Render Frame Rate:</strong> ${remoteVideoStatsMap[uid].RenderFrameRate} fps<br/>
+                            <strong>Render Resolution Heigh:</strong> ${remoteVideoStatsMap[uid].RenderResolutionHeight}px<br/>  
+                            <strong>Render Resolution Width:</strong> ${remoteVideoStatsMap[uid].RenderResolutionWidth}px<br/>
+                            <strong>Total Freeze Time:</strong> ${remoteVideoStatsMap[uid].TotalFreezeTime}s<br/>
+                            <strong>Total Play Duration:</strong> ${remoteVideoStatsMap[uid].TotalPlayDuration}s<br/>
+                            <strong>Transport Delay:</strong> ${remoteVideoStatsMap[uid].TransportDelay}ms
+                            `;
+            remoteVideoStatsBtn.data('bs.popover').element.dataset.content = videoStats;
+            remoteVideoStatsBtn.data('bs.popover').setContent();
+            remoteVideoStatsBtn.popover('update');
+        }
+      }
+    });
+  }, 1000);
+  statsIntervals.remoteVideo = remoteVideoInterval;
+}
+
+function disableStats() {
+  for(var interval in statsIntervals) {
+    try {
+      clearInterval(statsIntervals[interval]);
+    } catch (error) {
+      console(`error stoping interval: ${interval}`);
+      console(error);
+    }
+  }
+}
+
+
